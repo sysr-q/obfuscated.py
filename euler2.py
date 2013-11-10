922f8abe2b683652b0084cd783e03463489c47ef
plausibility <chris@gibsonsec.org>
2013-11-10 18:32:11 +1100

    This one's an eyesore.

---
+++ euler2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+chr, any, int = int, sum, chr
+def __(dict):
+    chr, type = ([0, dict()[0]]), dict(b=1)
+    long, int = chr
+    yield int
+    while int < type:
+        long, int = int, long + int
+        yield int
+a, long = ((lambda c="49", b=0.25: [chr(int(chr(c)))] if b == (1.0/4.0) else 4e6),
+           (lambda n: n % 2 == 0))
+dict = filter(long, __(a))
+print(any(dict))
