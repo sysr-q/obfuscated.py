04a2d2ad7f84b563b4694607da9b07c64b76afa8
plausibility <chris@gibsonsec.org>
2013-11-11 19:34:46 +1100

    I accidentally let Hitchhiker's Guide get into my head.

--- euler2.py
+++ euler2.py
@@ -5,24 +5,28 @@ import inspect
 
 __name__ = inspect.__name__
 globals()["stack"] = inspect.stack()[0][0].f_globals[(__file__ + __name__)[len(__file__):]].stack()[0]
-__file__, __name__ = [], "__builtins__"
+locals()["panic"] = lambda: len(__file__[-1])-1
+globals()["carrying_a_towel"] = lambda square: square % 2 == 0
+__file__, __name__ = [(("chr", "int"), ("any", "sum"), ("int", "chr"), ("foo", lambda: []))], "__builtins__"
 
-for val, key in (("chr", "int"), ("any", "sum"), ("int", "chr")):
+for dont, look in enumerate(__file__[0]):
+    val, key = look
+    if dont == panic():
+        globals().update({val: look[-1]})
+        continue
     stack[0].f_locals["()"] = inspect.stack()[0][0].f_globals["__builtins__"].__dict__[val]
     oops = vars()[repr(tuple())]
-    vars(stack.__getitem__(len(__file__)).f_globals[str(repr(__name__))[1:-1]])["list"] = vars(__builtins__)[key]
+    vars(stack.__getitem__(len(__file__) - len(__file__)).f_globals[str(repr(__name__))[1:-1]])["list"] = vars(__builtins__)[key]
     setattr(stack[0].f_globals[str(__name__)], key, oops)
     globals()[val] = list
 
-#chr, any, int = int, sum, chr
-def __(dict):
-    chr, type = ([0, dict()[0]]), dict(b=1)
-    long, int = chr
-    yield int
-    while int < type:
-        long, int = int, long + int
-        yield int
-a, long = ((lambda c="49", b=0.25: [chr(int(chr(c)))] if b == (1.0/4.0) else 4e6),
-           (lambda n: n % 2 == 0))
-dict = filter(long, __(a))
-print(any(dict))
+def hitch():
+    dont, panic = [0, 1], 4e6
+    always, carry = dont
+    a_towel = lambda yes, no="carry":yes[no]
+    yield carry
+    while carry < panic:
+        always, carry = carry, always + carry
+        yield a_towel(locals())
+
+print(any(filter(carrying_a_towel, hitch())))
